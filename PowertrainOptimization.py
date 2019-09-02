9d1457288a5ac300b994815cc149c5ccf502552d
nmstamps <35670640+nmstamps@users.noreply.github.com>
2019-09-02 13:52:42 -0400

    Created initial python files
    Main 2 python files created, simply containing filler script.

---
+++ PowertrainOptimization.py
@@ -0,0 +1,4 @@
+def main():
+	print("This is the main")
+	
+main()
---
+++ PowertrainSimulation.py
@@ -0,0 +1,10 @@
+'''
+This document is going to become the main scrip that will simulate and analyze a powertrain.
+This will likely need to be imported as if it were a library so parameters can be passed in. As a result, this script
+will not be able to run by itself.
+'''
+
+def main():
+    print("this is the main")
+
+main()
