a331298e0256bb9f78d5cec0f4813e9b030cf35c
sschoedel <sschoedel@vt.edu>
2019-09-03 17:58:46 -0400

    track simulator

--- TrackSim.py
+++ TrackSim.py
@@ -1,16 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from math import pi
 
 MPHoRPM = 0.019337455  # MPH/RPM
 MPSoRPM = 0.008644633  # meters/s/RPM
 GearRatio = 4
+WheelD = 0.6604 # Wheel diameter in meters
 
 CurrentMotor = 120000
 NewMotor = 120000
 tolerance = 10
 
-laps = input("Number of laps: ")  # this can be included in csv file in the future if we find that easier
+# laps = input("Number of laps: ")  # this can be included in csv file in the future if we find that easier
 
 file_name = r'\NJMP6_first4lapstwice.csv'
 file_path = r'C:\Users\sesch\Desktop\BOLT\Converted'
@@ -19,15 +21,14 @@ location = file_path + file_name
 data = pd.read_csv(location, names=['time', 'throttle', 'motorRPM', '4', '5', '6'], header=0, low_memory=False)
 data_df = pd.DataFrame(data)
 
-speed = np.empty(2)
+speed = np.ones(len(data_df['time']), np.float64)
 
-print(len(data['time']))
+for i in range(1, len(data_df['time'])):                           # add bike speed to array for every time instance
+    speed.put([i], data_df['motorRPM'][i] / 4 / 60 * WheelD * pi)  # speed in meters per second
+data_df['speed'] = speed  # add new column to DataFrame
+print(data_df)
 
-for i in range(0, len(data['time']-1)):
-    speed = np.append(data['motorRPM'][i]/4, axis=0)[i]  # add bike speed to array for every time instance
-print(len(speed))
-print(speed)
-data['speed'] = speed  # add new column to dataframe
+
 """
 time_offset = []
 for t in range(0, len(data)):
