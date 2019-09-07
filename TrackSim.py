55825b087086ac76b98b395a251a45d5fd295b15
sschoedel <sschoedel@vt.edu>
2019-09-06 23:46:41 -0400

    track sim

--- TrackSim.py
+++ TrackSim.py
@@ -6,7 +6,7 @@ from math import pi
 MPHoRPM = 0.019337455  # MPH/RPM
 MPSoRPM = 0.008644633  # meters/s/RPM
 GearRatio = 4
-WheelD = 0.6604 # Wheel diameter in meters
+WheelD = 0.6604  # Wheel diameter in meters
 
 CurrentMotor = 120000
 NewMotor = 120000
@@ -18,51 +18,60 @@ file_name = r'\NJMP6_first4lapstwice.csv'
 file_path = r'C:\Users\sesch\Desktop\BOLT\Converted'
 location = file_path + file_name
 
+# create DataFrame
 data = pd.read_csv(location, names=['time', 'throttle', 'motorRPM', '4', '5', '6'], header=0, low_memory=False)
 data_df = pd.DataFrame(data)
 
 # find race start time - needs to be more accurate
 start_time = 0
-for i in range(0, len(data['4'])):
+for i in range(0, len(data['time'])):
     if data['5'][i] >= 10:
         start_time = i
         break
 
-# add indexes to drop
-drop = np.ones(start_time, np.int8)
-for i in range(0, start_time):
-    drop.put([i], i)  # builds array for removing non-race data from data frame
-print(drop)
-for i in range(0, len(drop)):
-    data_trimmed = data_df.take(data_df.index[drop])  # removes non-race data
+# drop unused indexes
+data_trimmed = data_df.shift(-start_time)
+data_trimmed = data_trimmed.dropna()
 
-
-speed = np.ones(len(data_trimmed['time']), np.float64)
+# initialize arrays for new speed and distance data
 distance = np.ones(len(data_trimmed['time']), np.float64)
+speed = np.ones(len(data_trimmed['time']), np.float64)
+acceleration = np.ones(len(data_trimmed['time']), np.float64)
+accel_binary = np.ones(len(data_trimmed['time']), np.float64)
 
-for i in range(0, len(data_trimmed['time'])):                           # add bike speed to array for every time instance
+# calculate 'instantaneous' speed
+for i in range(start_time):
     speed.put([i], data_trimmed['motorRPM'][i] / 4 / 60 * WheelD * pi)  # speed in meters per second
-data_trimmed['speed'] = speed  # add new column to data frame
+data_trimmed['speed'] = speed  # add speed column to data frame
 
+# calculate aggregated distance
+# calculate 'instantaneous' acceleration
 for i in range(0, len(data_trimmed['time'])):
     if i == 0:
         distance.put([0], data_trimmed['speed'][0] * data_trimmed['time'][0] - 1)
+        acceleration.put([0], 0)
     else:
-        distance.put([i], data_trimmed['speed'][i] * (data_trimmed['time'][i] - data_trimmed['time'][i-1]))
-totalDistance = np.sum(distance, axis=0)  # total distance traveled on time interval in meters
-print('total distance over given time interval: {}'.format(totalDistance))
-print('All data: {}'.format(data_trimmed))
+        distance.put([i], distance[i-1] + data_trimmed['speed'][i] * (data_trimmed['time'][i] - data_trimmed['time'][i-1]))
+        acceleration.put([i], data_trimmed['speed'][i] - data_trimmed['speed'][i-1])
+data_trimmed['distance'] = distance  # add distance column to data frame
+data_trimmed['acceleration'] = acceleration  # add acceleration column to data frame
 
-"""
-time_offset = []
-for t in range(0, len(data)):
-    if data['pack_current'][t] >= 10:
-        start_time = t
-        break
-# print(len(data))
-plt.plot(data['time'], data['pack_current'])
+for i in range(0, len(data_trimmed['time'])):
+    if i == 0:
+        accel_binary.put([0], 0)
+    else:
+        if data_trimmed['speed'][i] > data_trimmed['speed'][i-1]:
+            accel_binary.put([i], 1)
+        else:
+            accel_binary.put([i], 0)
+data_trimmed['accel binary'] = accel_binary  # add binary acceleration column to data frame
 
-plt.ylabel('Pack Current')
+print('total distance over given time interval: {}'.format(distance[len(distance)-1]))
+print('All data:')
+print('{}'.format(data_trimmed))
+
+plt.plot(data_trimmed['time'], data_trimmed['acceleration'])
+
+plt.ylabel('Accel')
 plt.xlabel('Time')
 plt.show()
-"""
