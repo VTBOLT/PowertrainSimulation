6e6bf7fe880865a7793341ce98c06a54db33c9f6
sschoedel <sschoedel@vt.edu>
2019-09-18 00:21:11 -0400

    Now outputs final speed for each throttle segment

--- OriginalBikeModel.py
+++ OriginalBikeModel.py
@@ -6,6 +6,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from math import pi
 
+
+def get_distance(start, end):
+    for n in range(0, end - start):  # calculate the distance that throttle is held high
+        thisDistance = data_df['speed'][start + n] * (data_df['time'][start + n] - data_df['time'][
+            start + n - 1])  # calculate distance for one time interval
+        distance.put([n], thisDistance)  # add distance from a single time interval to distance array for this throttle
+    total_dist = np.sum(distance)
+    return total_dist
+
+
+def add_to_arrays(dists, final_speed):
+    dists.put([data_timeDist_counter - 1], total_distance)  # total distance over segment
+    final_speed.put([data_timeDist_counter - 1], data_df['speed'][all_i])  # speed at end of segment
+
+
 MPHoRPM = 0.019337455  # MPH/RPM
 MPSoRPM = 0.008644633  # meters/s/RPM
 GearRatio = 4
@@ -24,50 +39,75 @@ location = file_path + file_name
 # create DataFrame
 data = pd.read_csv(location, names=['time', 'throttle', 'motorRPM'], header=0, low_memory=False)
 data_df = pd.DataFrame(data)
-data_torqDist = pd.DataFrame(columns=['torque scale', 'distance'])
-
-start_time = 0
-# drop unused indexes
-data_trimmed = data_df.shift(-start_time)
-data_trimmed = data_trimmed.dropna()
+data_timeDist = pd.DataFrame(columns=['time start', 'high low', 'distances', 'final speed'])
 
 # initialize arrays for new speed and distance data
-speed = np.ones(len(data_trimmed['time']))
-distance = np.ones(len(data_trimmed['time']))
-throttle_binary = np.ones(len(data_trimmed['time']))
-torque_scaling = np.ones(len(data_trimmed['time']))
+speed = np.ones(len(data_df['time']))
+distance = np.ones(len(data_df['time']))
+torque_scaling = np.ones(len(data_df['time']))
 
-max_throttle = data_trimmed['throttle'].max()
+max_throttle = data_df['throttle'].max()
 
-# calculate 'instantaneous' speed
-for i in range(0, len(data_trimmed['time'])):
-    speed.put([i], data_trimmed['motorRPM'][i] / GearRatio / 60 * WheelD * pi)  # speed in meters per second
-data_trimmed['speed'] = speed  # add speed column to data frame
-
-# calculate aggregated distance
-for i in range(0, len(data_trimmed['time'])):
-    if i == 0:
-        distance.put([0], data_trimmed['speed'][0] * data_trimmed['time'][0] - 1)
-    else:
-        prevDistance = distance[i-1]
-        thisDistance = data_trimmed['speed'][i] * (data_trimmed['time'][i] - data_trimmed['time'][i-1])
-        distance.put([i], prevDistance + thisDistance)
-data_torqDist['distance'] = distance  # add distance column to final data frame
+# calculate instantaneous speed
+for i in range(0, len(data_df['time'])):
+    speed.put([i], data_df['motorRPM'][i] / GearRatio / 60 * WheelD * pi)  # speed in meters per second
+data_df['speed'] = speed  # add speed column to data frame
 
 # scale throttle values for use in finding new bike's torque
-for i in range(0, len(data_trimmed['time'])):
-    torque_scaling.put([i], data_trimmed['throttle'][i]/max_throttle)
-data_torqDist['torque scale'] = torque_scaling  # add scaled torque column to final data frame
+for i in range(0, len(data_df['time'])):
+    torque_scaling.put([i], data_df['throttle'][i] / max_throttle)  # add scaled torque to numpy array
 
-print('total distance over given time interval: {}'.format(distance[len(distance)-1]))
+time_start = np.ones(len(data_df['time']))
+high_low = np.ones(len(data_df['time']))
+distances = np.ones(len(data_df['time']))
+finalSpeedSeg = np.ones(len(data_df['time']))
+
+start_index = np.ones(len(data_df['time']))
+time_start_i = 0
+data_timeDist_counter = 0
+prevThrottleDown = False
+prevThrottle = False
+
+for all_i in range(0, len(data_df['time'])):
+    if torque_scaling[all_i] >= 0.5 and not prevThrottle:  # if throttle went high
+        prevThrottle = True
+        high_low.put([data_timeDist_counter], 2)  # throttle went high
+        time_start.put([data_timeDist_counter], data_df['time'][all_i])
+
+        if prevThrottleDown is True:  # find distance for previous low throttle and add to previous row
+            total_distance = get_distance(time_start_i, all_i)
+            add_to_arrays(distances, finalSpeedSeg)
+
+        data_timeDist_counter = data_timeDist_counter + 1
+        time_start_i = all_i
+
+    if torque_scaling[all_i] <= 0.5 and prevThrottle:  # if throttle went low
+        prevThrottleDown = True
+        prevThrottle = False
+        high_low.put([data_timeDist_counter], 0)  # throttle went low
+        time_start.put([data_timeDist_counter], data_df['time'][all_i])
+
+        total_distance = get_distance(time_start_i, all_i)  # find distance for high throttle and add to previous row
+        add_to_arrays(distances, finalSpeedSeg)
+
+        data_timeDist_counter = data_timeDist_counter + 1
+        time_start_i = all_i
+
+data_timeDist['time start'] = time_start
+data_timeDist['high low'] = high_low
+data_timeDist['distances'] = distances
+data_timeDist['final speed'] = finalSpeedSeg
+
+df_toRemove = data_timeDist[data_timeDist['time start'] == 1]
+data_timeDist = data_timeDist.drop(df_toRemove.index, axis=0)  # drop unnecessary
+
 print('All data:')
-print('{}'.format(data_trimmed))
+print('{}'.format(data_timeDist))
 
+plt.plot(data_timeDist['time start'], data_timeDist['final speed'])
 
-plt.plot(data_torqDist['distance'], data_torqDist['torque scaling'])
-
-plt.ylabel('Torque scaling')
-plt.xlabel('Distance')
+plt.ylabel('final speed')
+plt.xlabel('time start')
 plt.show()
 
 # still need intervals for acceleration vs braking so we know over what distance to scale the torque
--- TrackSim.py
+++ TrackSim.py
@@ -7,9 +7,23 @@ from math import pi
 
 # inputs: motor max torque and total bike mass
 
-MPHoRPM = 0.019337455  # MPH/RPM
-MPSoRPM = 0.008644633  # meters/s/RPM
-GearRatio = 4
-WheelR = 0.3302  # Wheel diameter in meters
+# from originalbikemodel.py: list of times rider throttles and for what distance they hold the throttle
+# also need max braking acceleration from bolt 3
 
-# for
+GearRatio = 4
+WheelR = 0.3302  # Wheel diameter in nanometers
+BikeM = 200  # mass input from optimization program in kgs
+max_torque = 10  # max torque of bike motor
+
+# torque = torque wheel = torque shaft * 4
+# f*r = torque
+# torque / r = f
+# f = m * a
+# torque_shaft * 4 / r / m = a
+
+max_accel = max_torque * GearRatio / WheelR / BikeM
+
+accelNew = np.ones(len(data_trimmed['time']))
+
+for i in range(0, len(data_trimmed['time'])):
+    accelNew.put([i], max_accel * data_trimmed['torque scaling'])
