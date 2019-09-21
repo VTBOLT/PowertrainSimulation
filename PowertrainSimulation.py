b6fc137a9f2f639728c1f40ea958132d2ec15841
sschoedel <sschoedel@vt.edu>
2019-09-20 23:43:06 -0400

    Removed unnecessary ignore lines

--- PowertrainSimulation.py
+++ PowertrainSimulation.py
@@ -1,5 +1,5 @@
 """
-This document is going to become the main scrip that will simulate and analyze a powertrain.
+This document is going to become the main scrip that will analyze a powertrain.
 This will likely need to be imported as if it were a library so parameters can be passed in. As a result, this script
 will not be able to run by itself.
 """
